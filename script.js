d550791f1173365a3040d9e82f56e8d27cc8e362
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-20 20:36:38 -0600

    error fixing

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const showMembers = document.querySelectorAll(".show-members");
 const pictures = document.querySelectorAll(".picture");
 const overlay = document.querySelector(".overlay");
 
+const close = function () {
+  overlay.classList.add("hidden");
+  for (let i = 0; i < pictures.length; i++) {
+    pictures[i].classList.add("hidden");
+  }
+};
+
 for (let i = 0; i < showMembers.length; i++) {
   showMembers[i].addEventListener("click", function () {
     pictures[i].classList.remove("hidden");
     overlay.classList.remove("hidden");
   });
 }
+
+overlay.addEventListener("click", close);
